ab7d95e6c392be8868ce1a0340ff9f29ecfedbcf
lizfrey <elizabeth.w.frey.24@dartmouth.edu>
2021-01-30 14:43:59 -0500

    added pictures

--- react-site/src/App.js
+++ react-site/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import ReactDOM from 'react-dom';
+import cambodia from './assets/cambodia-elephant.jpg';
+import me from './assets/ElizabethFrey.jpeg';
+import grad from './assets/IMG_7078.jpeg';
 
 function App(){
   var name;
@@ -36,6 +39,22 @@ function App(){
         <div className="sections">
             <h2>Images of Me</h2>
             <p>A few pictures of me and my family.</p>
+            <div class="sections" id="images">
+            <h2>Images</h2>
+            <p>Hover over each image to get a larger view.</p>
+            <div className="gallery">
+                <img src={me} alt="me"></img>
+                <div className="description">This is my senior portrait!</div>
+            </div>
+            <div className="gallery">
+                <img src={cambodia} alt="my family and me in Cambodia"></img>
+                <div className="description">This is me and my family in Cambodia.</div>
+            </div>
+            <div className="gallery">
+                <img src={grad} alt="my graduation"></img>
+                <div className="description">This is my high school graduation.</div>
+            </div>
+            </div>
         </div>
         <div className="sectionsNoLink">
             <h2>Contact Information</h2>
